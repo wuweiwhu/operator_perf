1cc6263b08f9e731da666be1a7086cdfb4d7af06
Eric <wuweiwhu@outlook.com>
2026-07-26 16:24:35 +0800

    Fix TMA cycles

--- gemm_case4.py
+++ gemm_case4.py
@@ -32,6 +32,7 @@ DDR_BW_PER_SM = 32
 DDR_UTIL = min(0.70, 224*0.8*3/(DDR_RT_LAT - L2_RT_LAT))
 FORCE_HIT = False
 STREAMING_STORE = False
+WRAM_UP = True
 PROLOGUE_CYCLES_EXTRA = 0000
 EPILOGUE_CYCLES_EXTRA = 0000
 
@@ -51,7 +52,7 @@ class CGA:
         coord_start_m = self.tile_m
         coord_start_n = self.tile_n
         #print(f"Processing Coord ({coord_start_m}, {coord_start_n})")
-        coord_start_k = tile_k * TILE_K
+        coord_start_k = tile_k
         A_L2C_Transfer_Bytes_Per_SM = L2.sizeof("A") / BLOCKS_IN_GGA
         A_NOC_Transfer_Bytes_Per_SM = A_L2C_Transfer_Bytes_Per_SM * MULTICAST_A
         A_DDR_Transfer_Bytes_Per_SM = 0
@@ -67,27 +68,31 @@ class CGA:
         L2C_Transfer_Bytes_Per_SM = A_L2C_Transfer_Bytes_Per_SM + B_L2C_Transfer_Bytes_Per_SM
         NOC_Transfer_Bytes_Per_SM = A_NOC_Transfer_Bytes_Per_SM + B_NOC_Transfer_Bytes_Per_SM
         DDR_Transfer_Bytes_Per_SM = A_DDR_Transfer_Bytes_Per_SM + B_DDR_Transfer_Bytes_Per_SM
-        Serilization_Cycles = max(L2C_Transfer_Bytes_Per_SM / (L2_RD_BW_PER_SM * L2_UTIL / (K_STAGE-1)), NOC_Transfer_Bytes_Per_SM / (NOC_RD_BW_PER_SM * NOC_UTIL / (K_STAGE-1)), DDR_Transfer_Bytes_Per_SM / (DDR_BW_PER_SM * DDR_UTIL / (K_STAGE-1)))
-        if A_hit and B_hit:
-            TMA_Cycles = Serilization_Cycles + L2_RT_LAT
-        else:
-            TMA_Cycles = Serilization_Cycles + DDR_RT_LAT
+
+        Serilization_Cycles = max(
+            L2C_Transfer_Bytes_Per_SM / (L2_RD_BW_PER_SM * L2_UTIL), 
+            NOC_Transfer_Bytes_Per_SM / (NOC_RD_BW_PER_SM * NOC_UTIL), 
+            DDR_Transfer_Bytes_Per_SM / (DDR_BW_PER_SM * DDR_UTIL)
+        )
+        RT_LAT = L2_RT_LAT if (A_hit and B_hit) else DDR_RT_LAT
+
+        request_issue_time = self.mma_cycles[tile_k % K_STAGE] + MBARRIER_SYNC_CYCLES
+        data_ready_time = request_issue_time + RT_LAT
+        bus_acquire_time = data_ready_time if (tile_k == 0) else max(data_ready_time, self.tma_cycles[(tile_k - 1) % K_STAGE])
+        self.tma_cycles[tile_k % K_STAGE] = bus_acquire_time + Serilization_Cycles
 
         MMA_Cycles = TILE_M_CGA * TILE_N_CGA * TILE_K / (SM_MMA_MACS * BLOCKS_IN_GGA * MMA_UTIL)
-
-        self.tma_cycles[tile_k % K_STAGE] = self.mma_cycles[tile_k % K_STAGE] + MBARRIER_SYNC_CYCLES + TMA_Cycles
-        #mma_idle_cycles = 0
-        #for stage in range(1, min(K_STAGE, tile_k+1)):
-        #    mma_idle_cycles = max(self.mma_cycles[(tile_k-stage) % K_STAGE], mma_idle_cycles)
         mma_idle_cycles = 0 if tile_k == 0 else self.mma_cycles[(tile_k - 1)%K_STAGE]
         self.mma_cycles[tile_k % K_STAGE] = max(self.tma_cycles[tile_k % K_STAGE], mma_idle_cycles) + MBARRIER_SYNC_CYCLES + MMA_Cycles
+        if self.cga_id == 0:
+            print(f"stage {tile_k} TMA: {self.tma_cycles[tile_k % K_STAGE]}, MMA: {self.mma_cycles[tile_k % K_STAGE]}")
     def done(self):
         return self.tile_m == None or self.tile_n == None
     def cycles(self):
         if self.done():
             return 0
-        coord_start_m = self.tile_m * TILE_M_CGA
-        coord_start_n = self.tile_n * TILE_N_CGA
+        coord_start_m = self.tile_m
+        coord_start_n = self.tile_n
         _, evict = L2.access("C", coord_start_m, coord_start_n)
         if not STREAMING_STORE:
             if evict > 0:
@@ -98,8 +103,8 @@ class CGA:
         else:
             C_Cycles = L2.sizeof("C") / BLOCKS_IN_GGA / (DDR_BW_PER_SM * DDR_UTIL)
         mainloop_cycles = max(max(self.tma_cycles), max(self.mma_cycles))
-        if self.cga_id == 0:
-            print(f"prologue: {PROLOGUE_CYCLES_EXTRA}, mainloop:{mainloop_cycles} cycles, epilogue:{C_Cycles + EPILOGUE_CYCLES_EXTRA} cycles")
+        #if self.cga_id == 0:
+            #print(f"prologue: {PROLOGUE_CYCLES_EXTRA}, mainloop:{mainloop_cycles} cycles, epilogue:{C_Cycles + EPILOGUE_CYCLES_EXTRA} cycles")
         Tile_Cycles = C_Cycles + MBARRIER_SYNC_CYCLES + mainloop_cycles + PROLOGUE_CYCLES_EXTRA+ EPILOGUE_CYCLES_EXTRA
         return Tile_Cycles
 
@@ -108,10 +113,15 @@ class L2CACHE:
     def __init__(self, size):
         self.size = size
         self.occupancy = 0
-        # cache 字典存储结构: {(data_type, start_X, start_Y): [last_access_timestamp, size_in_bytes]}
+        # cache: {(data_type, start_X, start_Y): [last_access_timestamp, size_in_bytes]}
         self.cache = dict()
         self.hit_count = 0
         self.access_count = 0
+        self.evict_class = {
+            "C": 0,
+            "A": 1,
+            "B": 1
+        }
 
     def sizeof(self, data_type, tile_m=TILE_M_CGA, tile_n=TILE_N_CGA, tile_k=TILE_K):
         dtype = {
@@ -135,6 +145,7 @@ class L2CACHE:
         req_size = self.sizeof(data_type, tile_m, tile_n, tile_k)
 
         if FORCE_HIT:
+            self.hit_count += 1
             return True, 0.0
         if STREAMING_STORE and data_type == "C":
             return False, 0.0
@@ -146,7 +157,7 @@ class L2CACHE:
 
         evicted_dirty_bytes = 0.0
         while self.occupancy + req_size > self.size and len(self.cache) > 0:
-            lru_key = min(self.cache, key=lambda k: self.cache[k][0])
+            lru_key = min(self.cache.keys(), key=lambda k: (self.evict_class[k[0]], self.cache[k][0]))
             _, evicted_size = self.cache[lru_key]
             self.occupancy -= evicted_size
             if lru_key[0] == "C":
@@ -158,7 +169,17 @@ class L2CACHE:
 
         return False, evicted_dirty_bytes
 
-L2 = L2CACHE(size=36 * 1024 * 1024 * 0.90)  # 36MB L2 cache, effective size 90% of total
+L2 = L2CACHE(size=36 * 1024 * 1024 * 0.90)
+
+if WRAM_UP:
+    for x in range(Prob_M // TILE_M_CGA):
+        for k in range(Prob_K // TILE_K):
+            L2.access("A", x, k)
+    for y in range(Prob_N // TILE_N_CGA):
+        for k in range(Prob_K // TILE_K):
+            L2.access("B", y, k)
+    L2.hit_count = 0
+    L2.access_count = 0
 
 def get_cga_tasks():
     for old_y in range(Prob_N // TILE_N_CGA):
@@ -191,15 +212,17 @@ while(True):
         for cluster in clusters:
             cluster.execute(tile_k)
     for cluster in clusters:
-        #print(cluster.cycles())
-        print(f"Execute: {cluster.tile_m} {cluster.tile_n}, Cycles: {cluster.cycles()}")
-        cluster.clock += cluster.cycles()
-        total_cycles = max(cluster.clock, total_cycles)
-        total_tile_cycles += cluster.cycles()
+        if not cluster.done():
+            cycles_this_tile = cluster.cycles()
+            print(f"Execute: {cluster.tile_m} {cluster.tile_n}, Cycles: {cycles_this_tile}")
+            cluster.clock += cycles_this_tile
+            total_cycles = max(cluster.clock, total_cycles)
+            total_tile_cycles += cycles_this_tile
 
 CGA_TILES = Prob_M // TILE_M_CGA * Prob_N // TILE_N_CGA
 Wave_Count = math.ceil(CGA_TILES / CLUSTER_COUNTS)
 total_avg_cycles = total_tile_cycles / CGA_TILES * Wave_Count
 print(f"Total Cycles: {total_cycles}")
 print(f"Total Avg Cycles: {total_avg_cycles}")
+#print(f"Tile Hit Rate: {L2.hit_count / max(1, L2.access_count) * 100:.2f}% (Hits: {L2.hit_count} / Total: {L2.access_count})")
 print(f"MMA Utilization: {Prob_M * Prob_N * Prob_K / (SM_MMA_MACS * SM_COUNTS) / total_cycles * 100}%")
